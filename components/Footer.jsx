9c0acf5d37285690d36ca9f33f15172953b80f3a
Abuh Solomon <53456402+Abuh05@users.noreply.github.com>
2022-08-26 16:53:19 +0200

    initial commit

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+
+import {UilFacebook, UilGithub, UilIstagram} from '@iconscout/react-unicons'
+import css from '../styles/Footer.module.css'
+
+export default function Footer( ) {
+    return (
+        <div className={css.container}>
+            <span>ALL RIGHT RESERVED</span>
+            <div className={css.social}>
+                <UilFacebook />
+                <UilGithub />
+                <UilIstagram />
+            </div>
+        </div>
+    )
+} 
